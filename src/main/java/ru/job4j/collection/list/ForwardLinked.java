fa510430a995fb903635916ce6c094716df6bade
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-06-15 23:31:31 +0300

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/list/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/list/ForwardLinked.java
@@ -3,13 +3,27 @@ package ru.job4j.collection.list;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Creates container based on LinkedList
+ *
+ * @author Veranika Lapianok
+ */
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private Node<T> tail;
 
+    /**
+     * Checks if a linkedList is empty
+     * @return true/false
+     */
     public boolean isEmpty() {
         return head == null;
     }
 
+    /**
+     * Adds an element to the linkedList
+     * @param value - value of an element to add (generic type)
+     */
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
@@ -23,10 +37,18 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    /**
+     * Adds the first element(head) to the linkedList
+     * @param value - value of an element to add (generic type)
+     */
     public void addFirst(T value) {
         head = new Node<>(value, head);
     }
 
+    /**
+     * Deletes the first element(head) from the linkedList
+     * @return value of the deleted element
+     */
     public T deleteFirst() {
         if (head == null) {
             throw new NoSuchElementException();
@@ -39,6 +61,33 @@ public class ForwardLinked<T> implements Iterable<T> {
         return tmpValue;
     }
 
+    /**
+     * Reverts the linkedList
+     * @return true/false
+     */
+    public boolean revert() {
+        boolean result = false;
+
+        if (!isEmpty() && head.next != null) {
+            tail = head;
+            Node<T> current = head.next;
+            head.next = null;
+
+            while (current != null) {
+               Node<T> next = current.next;
+               current.next = head;
+               head = current;
+               current = next;
+            }
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Creates iterator for the linkedList
+     * @return object Iterator
+     */
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
