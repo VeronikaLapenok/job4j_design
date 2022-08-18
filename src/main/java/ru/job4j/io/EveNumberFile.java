c3b40a57593536b35851db0140931b02534b2d46
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-08-18 15:25:59 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EveNumberFile.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EveNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            int number;
+            for (String line:lines) {
+                number = Integer.parseInt(line);
+                if (number % 2 == 0) {
+                    System.out.println(number);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
