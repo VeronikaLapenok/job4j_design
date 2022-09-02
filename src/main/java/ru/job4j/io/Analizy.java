760bfbb4c91209c8b99c7a00e202561987b96763
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-09-02 13:43:34 +0300

    2. Анализ доступности сервера. [#859 #338594]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,30 +1,22 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analizy {
     private boolean isAvailable = true;
     
     public void unavailable(String source, String target) {
-        List<String> targetList = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
             reader.lines().forEach(str -> {
                 if ((str.startsWith("400") || str.startsWith("500")) && isAvailable) {
-                    targetList.add(str.substring(4) + ";");
+                    writer.print(str.substring(4) + ";");
                     isAvailable = false;
                 } else if (str.startsWith("200") && !isAvailable) {
-                    targetList.add(str.substring(4) + ";" + System.lineSeparator());
+                    writer.print(str.substring(4) + ";" + System.lineSeparator());
                     isAvailable = true;
                 }
             });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            targetList.forEach(writer::print);
         } catch (IOException e) {
             e.printStackTrace();
         }
