87d90f35d26de555ca233ed662da236899901797
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2022-09-26 12:37:44 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Group.java
@@ -0,0 +1,31 @@
+package ru.job4j.serialization.json;
+
+import java.util.Arrays;
+
+public class Group {
+    private final String name;
+    private final int numOfStudents;
+    private final boolean isStudy;
+    private final Student student;
+    private final String[] subjects;
+
+    public Group(String name, int numOfStudents, boolean isStudy,
+                 Student student, String[] subjects) {
+        this.name = name;
+        this.numOfStudents = numOfStudents;
+        this.isStudy = isStudy;
+        this.student = student;
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{"
+                + "name='" + name + '\''
+                + ", numOfStudents=" + numOfStudents
+                + ", isStudy=" + isStudy
+                + ", student=" + student
+                + ", subjects=" + Arrays.toString(subjects)
+                + '}';
+    }
+}
