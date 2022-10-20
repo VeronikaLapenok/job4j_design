87bb3d279099ac43af481358cb7087bac8f50b0e
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2022-10-20 08:20:52 +0300

    2. Триггеры [#504803]

---
+++ src/main/java/ru/job4j/sql/triggers.sql
@@ -0,0 +1,76 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create or replace function tax_statement()
+   returns trigger as
+$$
+   BEGIN
+      update products
+      set price = price + price * 0.2
+      return new;
+   END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger_after
+   after insert on products
+   referencing new table as inserted
+   for each statement
+   execute procedure tax_statement();
+	
+insert into products (name, producer, price) values
+('milk', 'Milk farm', 15),
+('bread', 'Bread farm', 9),
+('meat', 'Meat farm', 40);
+
+create or replace function tax_row()
+   returns trigger as
+$$
+   BEGIN
+      update products
+      set price = price + price * 0.2;
+      return NEW;
+   END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger_before
+   before insert on products
+   for each row
+   execute procedure tax_row();
+	
+insert into products (name, producer, price) values
+('water', 'Water farm', 5);
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function insert_product_date()
+   returns trigger as
+$$
+   BEGIN
+      insert into history_of_price(name, price, date)
+      values (NEW.name, NEW.price, current_date);
+      return NEW;
+   END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger insert_products
+   after insert on products
+   for each row
+   execute procedure insert_product_date();
+   	
+insert into products (name, producer, price) values
+('coffe', 'Coffee farm', 49);
+
+select * from history_of_price;
